f4881af30ab9371c6bef1b00fad13737c4d2995c
Paulo-21 <paul.cibier@gmail.com>
2023-11-02 21:02:08 +0100

    continue with apx parser

--- src/graph.rs
+++ src/graph.rs
@@ -1,16 +1,19 @@
 use std::collections::HashMap;
 
 pub struct ArgumentationFramework {
-    af_attacker : Vec<i16>,
-	af_attackee : Vec<i16>,
-	argument_names : HashMap<String, i16>,
+    af_attacker : Vec<Vec<i32>>,
+	af_attackee : Vec<Vec<i32>>,
 }
 
 impl ArgumentationFramework {
     pub fn new(nb_arg : usize) -> Self {
-        let af_attackee = vec![0;nb_arg];
-        let af_attacker = vec![0;nb_arg];
-        let argument_names = HashMap::new();
-        Self { af_attackee , af_attacker, argument_names }
+        let af_attackee = vec![Vec::new();nb_arg];
+        let af_attacker = vec![Vec::new();nb_arg];
+        //let argument_names = HashMap::new();
+        Self { af_attackee , af_attacker}//, argument_names }
+    }
+    pub fn add_attack(&mut self, attacker : i32, target : i32) {
+        self.af_attacker[target as usize].push(attacker);
+        self.af_attackee[attacker as usize].push(target);
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -18,33 +18,60 @@ pub fn get_input(file_path : &str, format : Format) -> ArgumentationFramework {
 }
 
 fn readingAPX( file_path : &str) -> ArgumentationFramework {
+    
+    let nb_arg = find_number_argument(file_path);
+    let af = ArgumentationFramework::new(nb_arg as usize);
+
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
     let a = contents.trim().split('\n');
-    let b : Vec<&str> = a.collect();
-    let mut info = b[0].split_ascii_whitespace();
-    let p = info.next().unwrap();
-    let af = info.next().unwrap();
-    let n = info.next().unwrap().parse::<usize>().unwrap();
-    println!("{}", n);
-    let af = ArgumentationFramework::new(n);
+
+    for line in a {
+        if !line.starts_with("#") && (!line.trim().eq("")) {
+            af.add
+        }
+    }
+    
     af
 }
 fn readingCNF( file_path : &str) -> ArgumentationFramework {
     let contents = fs::read_to_string(file_path)
+    .expect("Should have been able to read the file");
+    let mut content_iter = contents.trim().split('\n');
+    let first_line = content_iter.next().unwrap();
+    let iter: Vec<&str> = first_line.split_ascii_whitespace().collect();
+    let nb_arg = iter[2].parse::<usize>().unwrap();
+    let mut af = ArgumentationFramework::new(nb_arg);
+
+    for line in content_iter {
+        if !line.starts_with('#') && (!line.trim().eq("")) {
+            let (attacker,target) = parseCNFAttackLine(line);
+            af.add_attack(attacker, target);
+        }
+    }
+    af
+}
+fn find_number_argument(file_path : &str) -> i32 {
+    let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
     let a = contents.trim().split('\n');
-    let first_line = a.next().unwrap();
-    let iter: Vec<&str> = first_line.split_ascii_whitespace().collect();
-    let nb_arg = iter[2].parse::<i32>().unwrap();
-    
-
-
+    let mut nb_arg = 0;
     for line in a {
-
+        if line.starts_with("arg") {
+            nb_arg +=1;
+        }
+        else {
+            break;
+        }
     }
-    let af = ArgumentationFramework::new(n);
-    af
+    nb_arg
+}
+
+fn parseCNFAttackLine (line : &str) -> (i32,i32) {
+    let mut a = line.split_ascii_whitespace();
+    let att = a.next().unwrap().parse::<i32>().unwrap();
+    let targ = a.next().unwrap().parse::<i32>().unwrap();
+    (att,targ)
 }
 
 fn print_supported_problems() {
