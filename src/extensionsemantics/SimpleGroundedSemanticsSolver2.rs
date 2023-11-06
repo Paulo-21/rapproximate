1cf2369f3fb0e6a05e0d051d2933d861d89ec708
Paulo-21 <paul.cibier@gmail.com>
2023-11-07 00:48:48 +0100

    Finish the second implémentation of SGSS

--- src/extensionsemantics/SimpleGroundedSemanticsSolver2.rs
+++ src/extensionsemantics/SimpleGroundedSemanticsSolver2.rs
@@ -8,57 +8,62 @@ use std::time::Instant;
 pub fn solve(af : &ArgumentationFramework, task : &Task) -> Vec<usize> {
 
     let problem_type = task.problem;
-    //let start = Instant::now();
-    let (mut label_in_final, mut label_undec) = initLabelling(af);
-    //println!("{}", start.elapsed().as_millis() as f32/1000.0);
+    let mut label_in_final = initLabelling2(af);
     let mut label_in_new = label_in_final.clone();
-    //stdout().flush();
     let mut label_out = vec![false;af.nb_argument];
+    let mut suspect_in = Vec::with_capacity(af.nb_argument);
     let mut hasChanged = true;
     while hasChanged {
         hasChanged = false;
         let mut n_label_in_new = Vec::with_capacity(af.nb_argument/10);
         for i in &label_in_new {
             for argument in &af.af_attackee[*i] {
-                label_out[*argument as usize] = true;
-                label_undec[*argument as usize] = false;
-                //label_undec.remove(&(*argument as usize));
-                //remove_element(&mut label_undec, *argument as usize);
-                hasChanged = true;
+                if !label_out[*argument as usize] {
+                    label_out[*argument as usize] = true;
+                    suspect_in.push(&af.af_attackee[*argument as usize]);
+                    hasChanged = true;
+                }
+            }   
+        }
+        for el in &suspect_in {
+            for index_of_suspect in &**el {
+                if allAttackersAreOut(af, &label_out, *index_of_suspect as usize) {
+                    n_label_in_new.push(*index_of_suspect as usize);
+                    label_in_final.push(*index_of_suspect as usize);
+                    hasChanged = true;
+                }
             }
         }
-        //let mut to_remove = Vec::with_capacity(af.nb_argument/2);
-        let mut i = 0;
-        for el in &mut label_undec {
-            if *el && allAttackersAreOut(af, &label_out, i) {
-                n_label_in_new.push(i);
-                label_in_final.push(i);
-                *el = false;
-                //to_remove.push(*el);
-                hasChanged = true;
-            }
-            i+=1;
-        }
-        /*for remove in to_remove {
-            label_undec.remove(&remove);
-        }*/
-        //label_in_new = n_label_in_new;
+        suspect_in.clear();
+        label_in_new = n_label_in_new;
     }
     label_in_final
 }
 
 
-fn initLabelling(af : &ArgumentationFramework) -> (Vec<usize>, Vec<bool>,) {
-    let mut label_in : Vec<usize> = Vec::with_capacity(af.nb_argument/2);
-    let mut label_undec = vec![false;af.nb_argument];
+fn initLabelling(af : &ArgumentationFramework) -> Vec<Label> {
+    let mut labelling : Vec<Label> = Vec::with_capacity(af.nb_argument);
+    //let mut label_undec = BTreeSet::new();
+    for i in 0..af.nb_argument {
+	    if af.af_attacker[i].is_empty() {
+            labelling.push(Label::IN);
+		}
+        else {
+            labelling.push(Label::UNDEC);
+        }
+	}
+    labelling
+}
+fn initLabelling2(af : &ArgumentationFramework) -> (Vec<usize>) {
+    let mut label_in : Vec<usize> = Vec::with_capacity(af.nb_argument);
+    
     //let mut label_undec = BTreeSet::new();
     for i in 0..af.nb_argument {
 	    if af.af_attacker[i].is_empty() {
 			label_in.push(i);
-		    label_undec[i] = false;
 		}
 	}
-    (label_in, label_undec)
+    (label_in)
 }
 
 fn allAttackersAreOut(af : &ArgumentationFramework, labelling : &Vec<bool>, index : usize) -> bool {
